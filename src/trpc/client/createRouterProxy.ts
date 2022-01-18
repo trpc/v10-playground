b9ea789c7c34e3ca12dea432856158d2c7753873
KATT <alexander@n1s.se>
2022-01-18 00:29:48 -0500

    type.path.type

--- src/trpc/client/createRouterProxy.ts
+++ src/trpc/client/createRouterProxy.ts
@@ -1,14 +1,11 @@
-import { ProceduresByType } from '../server';
+import type { ProceduresByType } from '../server';
 
 export function createRouterProxy<TRouter extends ProceduresByType<any>>() {
   return new Proxy({} as any, {
     get(_, type: string) {
       return new Proxy({} as any, {
         get(_, path: string) {
-          return {
-            type,
-            path,
-          };
+          return type + '.' + path;
         },
       });
     },
