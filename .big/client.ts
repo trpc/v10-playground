a0bf7082c32d54447326d1f9a949d5d768874874
KATT <alexander@n1s.se>
2022-01-22 00:14:30 -0500

    wip

--- .big/client.ts
+++ .big/client.ts
@@ -6,8 +6,8 @@ const client = createClient<typeof appRouter>();
 const { queries } = createRouterProxy<typeof appRouter>();
 
 async function main() {
-  // const q1 = await client.query(queries.r99q5, { hello: 'world' });
-  const q1 = await client.query('r99q5', { hello: 'world' });
+  const q1 = await client.query(queries.r99q5, { hello: 'world' });
+  // const q1 = await client.query('r99q5', { hello: 'world' });
 
   if ('data' in q1) {
     console.log('data.input', q1.data.input);
