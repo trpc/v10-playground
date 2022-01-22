1ff98572de1bb37133d410aa7b1805196e0898e1
KATT <alexander@n1s.se>
2022-01-21 22:57:12 -0500

    router 99 test

--- .big/client.ts
+++ .big/client.ts
@@ -6,8 +6,8 @@ const client = createClient<typeof appRouter>();
 const { queries } = createRouterProxy<typeof appRouter>();
 
 async function main() {
-  const greeting = await client.query(queries.r42q5, { hello: 'world' });
-  const posts = await client.query('r42q5', { hello: 'world' });
+  const greeting = await client.query(queries.r99q5, { hello: 'world' });
+  const posts = await client.query('r99q5', { hello: 'world' });
 
   console.log({ greeting, posts });
 }
--- .big/server/routers/router99.ts
+++ .big/server/routers/router99.ts
@@ -1,210 +1,206 @@
 import { trpc } from '../context';
 import { z } from 'zod';
 
-
 export const router99 = trpc.router({
   queries: {
-    
-  
-r99q0: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+    r99q0: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q1: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q1: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q2: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q2: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q3: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q3: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q4: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q4: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q5: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q5: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q6: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q6: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q7: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q7: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q8: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q8: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-r99q9: trpc.resolver(
-    trpc.zod(
-      z.object({
-        hello: z.string(),
-        lengthOf: z
-          .string()
-          .transform((s) => s.length)
-          .optional()
-          .default(''),
-      }),
+
+    r99q9: trpc.resolver(
+      trpc.zod(
+        z.object({
+          hello: z.string(),
+          lengthOf: z
+            .string()
+            .transform((s) => s.length)
+            .optional()
+            .default(''),
+        }),
+      ),
+      (params) => {
+        return {
+          data: {
+            input: params.input,
+          },
+        };
+      },
     ),
-    (params) => {
-      return {
-        data: {
-          input: params.input,
-        }
-      }
-    }
-  ),
-  
-  }
+  },
 });
