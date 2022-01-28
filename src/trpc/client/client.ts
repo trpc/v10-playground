28140fb255544664e7155684ba2c35adaf067baa
KATT <alexander@n1s.se>
2022-01-28 10:32:46 +0100

    fix

--- src/trpc/client/client.ts
+++ src/trpc/client/client.ts
@@ -1,6 +1,6 @@
 import type { ProceduresByType } from '../server';
 
-export function createRouterProxy<TRouter extends ProceduresByType<any>>() {
+function createRouterProxy<TRouter extends ProceduresByType<any>>() {
   return new Proxy({} as any, {
     get(_, type: string) {
       return new Proxy({} as any, {
