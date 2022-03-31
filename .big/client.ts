f09f0097c9129e80618a8627f0b8f5a7fd459737
KATT <alexander@n1s.se>
2022-03-31 21:58:05 +0200

    fix

--- .big/client.ts
+++ .big/client.ts
@@ -6,11 +6,7 @@ const client = createClient<typeof appRouter>();
 async function main() {
   const result = await client.query.r0q0({ hello: 'world' });
 
-  if (result.ok) {
-    console.log(result.data);
-  } else {
-    console.log(result.error.code);
-  }
+  console.log(result.data);
 }
 
 main();
