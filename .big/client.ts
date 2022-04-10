28d4c7cea3971ad6c4420de37fe3a4a94ddf182b
KATT <alexander@n1s.se>
2022-04-10 18:32:08 +0200

    input

--- .big/client.ts
+++ .big/client.ts
@@ -4,7 +4,7 @@ import type { appRouter } from './server/routers/_app';
 const client = createClient<typeof appRouter>();
 
 async function main() {
-  const result = await client.query.r0q0({ hello: 'world' });
+  const result = await client.query.r0q0({ input: { hello: 'world' } });
 
   console.log(result);
 }
