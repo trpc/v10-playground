0bae38193728d94b3b2637c9fae3670ca55243b5
KATT <alexander@n1s.se>
2022-04-11 10:52:01 +0200

    structure

--- src/server/routers/mixedRouter.ts
+++ src/server/routers/mixedRouter.ts
@@ -1,37 +1,7 @@
 import { z } from 'zod';
 import { isAuthed, procedure, trpc } from '../context';
 
-/**
- * A reusable combination of an input + middleware that can be reused.
- * Accepts a Zod-schema as a generic.
- */
-export function isPartofOrg<
-  TSchema extends z.ZodObject<{ organizationId: z.ZodString }>,
->(schema: TSchema) {
-  return procedure.input(schema).use((params) => {
-    const { ctx, input } = params;
-    const { user } = ctx;
-    if (!user) {
-      throw new Error('UNAUTHORIZED');
-    }
-
-    if (
-      !user.memberships.some(
-        (membership) => membership.organizationId !== input.organizationId,
-      )
-    ) {
-      throw new Error('FORBIDDEN');
-    }
-
-    return params.next({
-      ctx: {
-        user,
-      },
-    });
-  });
-}
 // Router with some mixed procedures
-
 export const mixedRouter = trpc.router({
   queries: {
     // procedure with input validation called `greeting`
@@ -63,34 +33,6 @@ export const mixedRouter = trpc.router({
     fireAndForget: procedure.input(z.string()).resolve(() => {
       // no return
     }),
-    editOrg: procedure
-      .concat(
-        isPartofOrg(
-          z.object({
-            organizationId: z.string(),
-            data: z
-              .object({
-                name: z.string(),
-              })
-              .partial(),
-          }),
-        ),
-      )
-      .resolve(({ input }) => {
-        // - User is guaranteed to be part of the organization queried
-        //-  `input` is of type:
-        // {
-        //   data: {
-        //       name: string;
-        //   };
-        //   organizationId: string;
-        // }
-        // [.... logic]
-        return {
-          id: input.organizationId,
-          ...input.data,
-        };
-      }),
 
     updateTokenHappy: procedure
       .input(z.string())
